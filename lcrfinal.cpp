717b2b0b8d8fa34d0cb8cbd075189a33df91ffd7
Vani2130 <144881823+Vani2130@users.noreply.github.com>
2024-05-22 01:46:25 +0530

    Update lcrfinal.cpp

--- lcrfinal.cpp
+++ lcrfinal.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include<fstream>
+#include<cmath>
+using namespace std;
+void RungeKutta(float &q, float &i, float dt, float L,float C, float R){
+  k1_q=i;
+  k1_i=(-R*i-q/C)/L;
 
+  k2_q=i+0.5*dt*k1_i;
+  k2_i=(-R*(i+0.5*dt*k1_i)-(q+0.5*dt*k1_q)/C)/L;
+  
